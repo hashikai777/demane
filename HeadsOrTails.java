967a4566947500cee1bc28eb786589b035a5bb72
HASHIMOTO Kai <s1310083@sshsv211.u-aizu.ac.jp>
2024-06-28 12:20:44 +0900

    Final revision

--- HeadsOrTails.java
+++ HeadsOrTails.java
@@ -3,6 +3,7 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class HeadsOrTails {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Random rand = new Random();
