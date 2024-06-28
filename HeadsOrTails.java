aeda61206f0c21cc537bf6a12fb7cfbedeacab0d
HASHIMOTO Kai <s1310083@sshsv211.u-aizu.ac.jp>
2024-06-28 12:27:48 +0900

    Victory message added

--- HeadsOrTails.java
+++ HeadsOrTails.java
@@ -3,7 +3,6 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class HeadsOrTails {
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Random rand = new Random();
@@ -28,5 +27,13 @@ public class HeadsOrTails {
         }
 
         System.out.println("Heads: " + heads + ", Tails: " + tails);
+
+        if (heads > tails) {
+            System.out.println(name + " won!");
+        } else {
+            System.out.println(name + " lost!");
+        }
+
+        scanner.close();
     }
 }
